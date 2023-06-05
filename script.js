9ac086df419b21ef6bdeb8c93ba3a1acde113b0f
Peter Quinn <peterdquinnsr@gmail.com>
2023-06-05 16:10:36 -0700

    finished 1

--- script.js
+++ script.js
@@ -1,41 +1,71 @@
 document.getElementById('employee-form').addEventListener('submit', addEmployee);
+document.getElementById('search-button').addEventListener('click', searchEmployees);
 
-const employees = [
-    {name: "Alice", role: "Senior Developer", skills: ["JavaScript", "HTML", "CSS"]},
-    {name: "Bob", role: "Senior Developer", skills: ["Python", "Java", "C++"]},
-    {name: "Charlie", role: "Senior Designer", skills: ["UX/UI Design", "HTML", "CSS"]},
-    {name: "Diana", role: "Senior Designer", skills: ["UX/UI Design", "JavaScript", "React"]},
-    // add more employee objects...
-];
+let employees = [];
+
+// Check if employee data exists in local storage
+const storedEmployees = localStorage.getItem('employees');
+if (storedEmployees) {
+  employees = JSON.parse(storedEmployees);
+} else {
+  // If no data exists, initialize with default employees
+  employees = [
+    { name: "Alice", role: "Senior Developer", skills: ["JavaScript", "HTML", "CSS"] },
+    { name: "Bob", role: "Senior Developer", skills: ["Python", "Java", "C++"] },
+    { name: "Charlie", role: "Senior Designer", skills: ["UX/UI Design", "HTML", "CSS"] },
+    { name: "Diana", role: "Senior Designer", skills: ["UX/UI Design", "JavaScript", "React"] }
+  ];
+}
 
 function addEmployee(event) {
-    event.preventDefault();
+  event.preventDefault();
 
-    const name = document.getElementById('name').value;
-    const role = document.getElementById('role').value;
-    const skills = document.getElementById('skills').value.split(',');
+  const name = document.getElementById('name').value;
+  const role = document.getElementById('role').value;
+  const skills = document.getElementById('skills').value.split(',');
 
-    const newEmployee = {name: name, role: role, skills: skills};
-    employees.push(newEmployee);
+  const newEmployee = { name: name, role: role, skills: skills };
+  employees.push(newEmployee);
 
-    updateEmployeeList();
+  updateEmployeeList();
+  saveEmployeesToLocalStorage();
 }
 
 function updateEmployeeList() {
-    const list = document.getElementById('employee-list');
-    list.innerHTML = '';
+  const list = document.getElementById('employee-list');
+  list.innerHTML = '';
 
-    for (let i = 0; i < employees.length; i++) {
-        const li = document.createElement('li');
-        li.innerHTML = `<h2>${employees[i].name}</h2><p>${employees[i].role}</p><p>${employees[i].skills.join(', ')}</p><button onclick="deleteEmployee(${i})">Delete</button>`;
-        list.appendChild(li);
-    }
+  for (let i = 0; i < employees.length; i++) {
+    const employee = employees[i];
+    const li = document.createElement('li');
+    li.innerHTML = `
+      <h2>${employee.name}</h2>
+      <p>${employee.role}</p>
+      <p>${employee.skills.join(', ')}</p>
+    `;
+    list.appendChild(li);
+  }
 }
 
-function deleteEmployee(index) {
-    employees.splice(index, 1);
-    updateEmployeeList();
+function searchEmployees() {
+  const searchInput = document.getElementById('search-input').value.toLowerCase();
+  const filteredEmployees = employees.filter(employee =>
+    employee.name.toLowerCase().includes(searchInput) ||
+    employee.role.toLowerCase().includes(searchInput) ||
+    employee.skills.some(skill => skill.toLowerCase().includes(searchInput))
+  );
+
+  if (filteredEmployees.length > 0) {
+    employees = filteredEmployees;
+  } else {
+    employees = [];
+  }
+
+  updateEmployeeList();
 }
 
-// Populate the list with the initial employees
+function saveEmployeesToLocalStorage() {
+  localStorage.setItem('employees', JSON.stringify(employees));
+}
+
 updateEmployeeList();
